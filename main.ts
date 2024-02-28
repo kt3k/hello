d9ed26a53ff7adee4a12799cbbcfb8fb04763dcc
Yoshiya Hinosawa <stibium121@gmail.com>
2024-02-28 13:00:27 +0900

    chore: update

--- main.ts
+++ main.ts
@@ -1,8 +1,6 @@
-export function add(a: number, b: number): number {
-  return a + b;
+export function hello() {
+  console.log("hello");
 }
-
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  hello();
 }
