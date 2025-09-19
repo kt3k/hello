6eef1daa8bba17a37639eea7a992b4dcd8f315e5
Yoshiya Hinosawa <stibium121@gmail.com>
2025-09-19 13:50:56 +0900

    test declaring type

--- main.ts
+++ main.ts
@@ -2,6 +2,9 @@
 export function hello() {
   console.log("hello");
 }
+
+declare const Deno: { unrefTimer(id: number): void };
+
 if (import.meta.main) {
   hello();
   Deno.serve(() => new Response("hello"));
